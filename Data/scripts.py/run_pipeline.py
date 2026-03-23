0cedfe497b0a74aac5fd72821a0b3e26e4e51cd2
t95532 <tarun.95532@gmail.com>
2026-03-23 22:12:41 +0530

    renamed folder name

--- Data/scripts.py/run_pipeline.py
+++
@@ -1,9 +0,0 @@
-from src.pipeline_pkg.pipeline import run_pipeline
-
-if __name__ == "__main__":
-    success = run_pipeline()
-
-    if success:
-        print("Run completed")
-    else:
-        print("Skipped (already ran today)")
