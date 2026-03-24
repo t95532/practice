5f126ee123c55f6dd15d1ede2800f4090436a33f
t95532 <tarun.95532@gmail.com>
2026-03-24 15:15:14 +0530

    pipe line set up completed

--- Data/src/pipeline_pkg/model.py
+++ Data/src/pipeline_pkg/model.py
@@ -3,12 +3,12 @@ from .config import MODEL_PATH, SCALER_PATH
 
 def load_model():
     with open(str(MODEL_PATH), "rb") as f:
-        print(f"Model loaded from: {MODEL_PATH}")
+        #print(f"Model loaded from: {MODEL_PATH}")
         return pickle.load(f)
 
 def load_scaler():
     with open(str(SCALER_PATH), "rb") as f:
-        print(f"Scaler loaded from: {SCALER_PATH}")
+        #print(f"Scaler loaded from: {SCALER_PATH}")
         return joblib.load(f)
 
 def predict(model, df):
